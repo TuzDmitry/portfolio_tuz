6ad980824f53cf4b3bd674136b3419b016154b82
TuzDmitry <dimatuz94@gmail.com>
2020-03-13 02:23:34 +0300

    made start structure: parts of portfolio

---
+++ src/Projects/Projects.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Projects.module.css';
+
+function Projects() {
+  return (
+    <div className={styles.projects}>
+      здесь живут проджектс
+    </div>
+  );
+}
+
+export default Projects;
---
+++ src/Slogan/Slogan.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Slogan.module.css';
+
+function Slogan() {
+  return (
+    <div className={styles.slogan}>
+      здесь живёт слоган
+    </div>
+  );
+}
+
+export default Slogan;
