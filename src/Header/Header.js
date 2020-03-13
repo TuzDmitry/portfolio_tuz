f6dcfb0168731cd7a2d002496b04a02712a6ff53
TuzDmitry <dimatuz94@gmail.com>
2020-03-13 03:30:25 +0300

    imposed: Header and Main parts

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import styles from './Header.module.css';
+import NavMenu from "./NavMenu/NavMenu";
 
 function Header() {
   return (
     <div className={styles.header}>
-        здесь живет хедер
+        <div className={styles.container}>
+            <NavMenu />
+        </div>
     </div>
   );
 }
--- src/Main/Main.js
+++ src/Main/Main.js
@@ -4,7 +4,16 @@ import styles from './Main.module.css';
 function Main() {
   return (
     <div className={styles.main}>
-        здесь живет мэин
+        <div className={styles.container}>
+            <div className={styles.greeting}>
+                <div>Привет</div>
+                <div>Меня зовут Дмитрий Туз</div>
+                <div>Я React разработчик</div>
+            </div>
+            <div className={styles.myPhoto}>
+                <img src="https://filmy.by/wp-content/uploads/2019/10/david-duchovny-filmy-by.jpg" alt=""/>
+            </div>
+        </div>
     </div>
   );
 }
