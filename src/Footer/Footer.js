81e7a14ad491e9dc8e7806ed0eefb9d6993f8e20
TuzDmitry <dimatuz94@gmail.com>
2020-03-13 16:17:27 +0300

    imposed: Footer parts

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -4,7 +4,15 @@ import styles from './Footer.module.css';
 function Footer() {
   return (
     <div className={styles.footer}>
-      здесь живёт футер
+        <div className={styles.container}>
+            <div>Туз Дмитрий</div>
+            <div className={styles.social}>
+                <a href=""><img src="https://toy-boy.ru/images/vkontakte.png" alt="VK"/></a>
+                <a href=""><img src="https://zberovski.ru/wp-content/uploads/2018/11/Facebook-logo-icon-vectorcopy-big_copy-1020x1024.png" alt="FaceBook"/></a>
+                <a href=""><img src="https://www.clipartmax.com/png/full/62-624588_linkedin-help-linkedin-logo-2018-png.png" alt="Linkedin"/></a>
+            </div>
+            <div className={styles.copyright}>© 2020, All Rights Reserved</div>
+        </div>
     </div>
   );
 }
