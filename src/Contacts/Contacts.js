e3dd4c73a643d10046c3bfdc86eb4f8ae637e224
TuzDmitry <dimatuz94@gmail.com>
2020-03-13 15:55:53 +0300

    imposed: Contacts parts

--- src/Contacts/Contacts.js
+++ src/Contacts/Contacts.js
@@ -2,11 +2,19 @@ import React from 'react';
 import styles from './Contacts.module.css';
 
 function Contacts() {
-  return (
-    <div className={styles.contacts}>
-      здесь живут контакты
-    </div>
-  );
+    return (
+        <div className={styles.contacts}>
+            <div className={styles.container}>
+                <form className={styles.form} action="">
+                    <input type="text" value="Name"/>
+                    <input type="text" value="E-mail"/>
+                    <textarea name="" id="" cols="30" rows="10"  value="Your message"></textarea>
+                    <button>Нанять меня</button>
+                </form>
+
+            </div>
+        </div>
+    );
 }
 
 export default Contacts;
