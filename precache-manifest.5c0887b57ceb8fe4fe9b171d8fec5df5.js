34acbe66462a973388da4b54daf7276f006bb7cf
TuzDmitry <dimatuz94@gmail.com>
2020-03-31 01:44:30 +0300

    Updates

---
+++ precache-manifest.5c0887b57ceb8fe4fe9b171d8fec5df5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "934b84e00897e032de5d1f883573a8aa",
+    "url": "/portfolio_tuz/index.html"
+  },
+  {
+    "revision": "4381de4b7175f374a68d",
+    "url": "/portfolio_tuz/static/css/main.8637e7f1.chunk.css"
+  },
+  {
+    "revision": "4960bd9cd78871e2ae8d",
+    "url": "/portfolio_tuz/static/js/2.dda25eda.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/portfolio_tuz/static/js/2.dda25eda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4381de4b7175f374a68d",
+    "url": "/portfolio_tuz/static/js/main.5105c641.chunk.js"
+  },
+  {
+    "revision": "e97b631bb12a6dd0e9b3",
+    "url": "/portfolio_tuz/static/js/runtime-main.ae29e335.js"
+  }
+]);
