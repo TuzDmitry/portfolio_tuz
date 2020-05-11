eb96d1da84d5fdfaaeb57ac3c1730221b582f568
TuzDmitry <dimatuz94@gmail.com>
2020-05-11 18:18:47 +0300

    Updates

---
+++ precache-manifest.91489b6291f867a766320bdd4e4850ed.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9895ab74e3b1cbed0c3455d0bdfd335a",
+    "url": "/portfolio_tuz/index.html"
+  },
+  {
+    "revision": "5b81a694d64b47522584",
+    "url": "/portfolio_tuz/static/css/main.e4d0f7c3.chunk.css"
+  },
+  {
+    "revision": "5c4f0bf5671fd3b1fb98",
+    "url": "/portfolio_tuz/static/js/2.595472af.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/portfolio_tuz/static/js/2.595472af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b81a694d64b47522584",
+    "url": "/portfolio_tuz/static/js/main.06a24756.chunk.js"
+  },
+  {
+    "revision": "e97b631bb12a6dd0e9b3",
+    "url": "/portfolio_tuz/static/js/runtime-main.ae29e335.js"
+  },
+  {
+    "revision": "1c088c20ad34b47ae384696ef3ebdcfd",
+    "url": "/portfolio_tuz/static/media/Html5_Css3_JS_React_Angular_Vue.1c088c20.svg"
+  },
+  {
+    "revision": "ddbb6e6fa9d6a392b48b057f44933398",
+    "url": "/portfolio_tuz/static/media/css.ddbb6e6f.png"
+  },
+  {
+    "revision": "1894126bedf3336f265240d153a6c11f",
+    "url": "/portfolio_tuz/static/media/html.1894126b.png"
+  },
+  {
+    "revision": "35b5cbd53c003d639f157a554788617b",
+    "url": "/portfolio_tuz/static/media/js.35b5cbd5.png"
+  },
+  {
+    "revision": "ba8db61aa45de073572861a60194909e",
+    "url": "/portfolio_tuz/static/media/my_main_photo.ba8db61a.jpg"
+  },
+  {
+    "revision": "0e5f617c6cff3c9e60a76e75dc99e203",
+    "url": "/portfolio_tuz/static/media/project_1.0e5f617c.jpg"
+  },
+  {
+    "revision": "e6014240bb8cb2a8278c72eda1f40a71",
+    "url": "/portfolio_tuz/static/media/project_2.e6014240.jpg"
+  },
+  {
+    "revision": "3dcdfcdd7d5a5c97122f5d4223b97cb2",
+    "url": "/portfolio_tuz/static/media/project_3.3dcdfcdd.jpg"
+  },
+  {
+    "revision": "0d5b04380dd13e66522ca900e28a86ce",
+    "url": "/portfolio_tuz/static/media/project_4.0d5b0438.jpg"
+  },
+  {
+    "revision": "84331f10b4148ffb813cdafc281b1469",
+    "url": "/portfolio_tuz/static/media/react.84331f10.png"
+  }
+]);
